a4e9c09a43a9f1bcd631dd2845a40b0294ef7cdc
Cosmin Stoica <cosmin.stoica97@gmail.com>
2023-12-04 01:52:49 +0200

    added: day 3

---
+++ 2023/dec3/part1.ts
@@ -0,0 +1,31 @@
+import fs from 'fs/promises';
+
+const isSymbol = /[^.0-9]/;
+const numbers = /\d+/g;
+
+async function part1() {
+    const buffer = await fs.readFile('dec3/part1.input');
+    const lines = buffer.toString('utf-8').split(/\n/);
+
+    return lines.reduce((sum, line, currentIndex) => {
+        for (const match of line.matchAll(numbers)) {
+            const [numberAsString] = match;
+            const { index } = match;
+
+            const boundaryLeft = index - 1;
+            const boundaryRight = index + numberAsString.length + 1;
+
+            const hasSymbolOnSameLine = isSymbol.test(line.charAt(boundaryLeft)) || isSymbol.test(line.charAt(boundaryRight - 1));
+            const hasSymbolOnPrevLine = lines[currentIndex - 1] && isSymbol.test(lines[currentIndex - 1].substring(boundaryLeft, boundaryRight));
+            const hasSymbolOnNextLine = lines[currentIndex + 1] && isSymbol.test(lines[currentIndex + 1].substring(boundaryLeft, boundaryRight));
+
+            if (hasSymbolOnSameLine || hasSymbolOnPrevLine || hasSymbolOnNextLine) {
+                sum += Number(numberAsString);
+            }
+        }
+
+        return sum;
+    }, 0);
+}
+
+part1().then(console.log);
---
+++ 2023/dec3/part2.ts
@@ -0,0 +1,56 @@
+import fs from 'fs/promises';
+
+const gears = /\*/g;
+const numbers = /\d+/g;
+
+async function part2() {
+    const buffer = await fs.readFile('dec3/part2.input');
+    const lines = buffer.toString('utf-8').split(/\n/);
+
+    const allNumbers = lines.map(line => {
+        const result = [] as { value: string, start: number, end: number }[];
+
+        for (const match of line.matchAll(numbers)) {
+            result.push({
+                value: match[0],
+                start: match.index,
+                end: match.index + match[0].length - 1,
+            });
+        }
+
+        return result;
+    }, []);
+
+    return lines.reduce((sum, line, idx) => {
+        for (const { index: gearAt } of line.matchAll(gears)) {
+            const currentLineNeighbours = allNumbers[idx].filter(someNumber => {
+                return someNumber.start === gearAt + 1 || someNumber.end === gearAt - 1;
+            });
+
+            const prevLineNeighbours = allNumbers[idx - 1]?.filter(someNumber => {
+                return Math.abs(someNumber.start - gearAt) <= 1
+                    || Math.abs(someNumber.end - gearAt) <= 1
+                    || (gearAt >= someNumber.start && gearAt <= someNumber.end); // also handles 4+ digits numbers
+            }) ?? [];
+
+            const nextLineNeighbours = allNumbers[idx + 1]?.filter(someNumber => {
+                return Math.abs(someNumber.start - gearAt) <= 1
+                    || Math.abs(someNumber.end - gearAt) <= 1
+                    || (gearAt >= someNumber.start && gearAt <= someNumber.end); // also handles 4+ digits numbers
+            }) ?? [];
+
+            const totalNeighbours = [
+                ...prevLineNeighbours,
+                ...currentLineNeighbours,
+                ...nextLineNeighbours,
+            ];
+
+            if (totalNeighbours.length === 2)
+                sum += Number(totalNeighbours[0].value) * Number(totalNeighbours[1].value);
+        }
+
+        return sum;
+    }, 0);
+}
+
+part2().then(console.log);
